b6c073aebdfb371647a6f661ad8fff0691c9e336
Woodwood1299 <jmtd242@gmail.com>
2025-09-15 19:42:39 +0100

    Announcement Collection Done

--- fenixgoscraper.go
+++ fenixgoscraper.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Announcement struct {
-	Link string
+	Link, Message string
 }
 
 func ScrapeOut() {
@@ -16,29 +16,40 @@ func ScrapeOut() {
 
 func Scrape() {
 	fmt.Print("Running\n")
+	links := [4]string{
+		"https://fenix.tecnico.ulisboa.pt/disciplinas/Apre2222/2025-2026/1-semestre/anuncios",
+		"https://fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/anuncios",
+		"https://fenix.tecnico.ulisboa.pt/disciplinas/Mod112/2025-2026/1-semestre/anuncios",
+		"https://fenix.tecnico.ulisboa.pt/disciplinas/RC112/2025-2026/1-semestre/anuncios",
+	}
 
 	var announcements []Announcement
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("fenix.tecnico.ulisboa.pt/disciplinas"))
+		colly.AllowedDomains("fenix.tecnico.ulisboa.pt"),
+	)
 
-	c.OnHTML("h5[style=margin-top: 0; font-weight:400]", func(h *colly.HTMLElement) {
-		fmt.Print("OnHTML\n")
+	c.OnHTML("h5[style]", func(h *colly.HTMLElement) {
 		announcement := Announcement{}
-
 		announcement.Link = h.ChildAttr("a", "href")
-
+		announcement.Message = h.Text
 		announcements = append(announcements, announcement)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
-		fmt.Print("OnScraped\n")
-		for i := 0; i < len(announcements); i++ {
-			fmt.Print(announcements[i].Link)
+		previous_link := "https://fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/ver-post/planeamento-e-lab-1https://fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/ver-post/aula-teorica-de-substituicao-ja-disponivel-online"
+
+		for _, a := range announcements {
+			if a.Link != previous_link {
+				fmt.Printf("- %s\n\t%s\n\n", a.Message, a.Link)
+			}
 		}
 	})
 
-	fmt.Print("Visiting\n")
-	c.Visit("fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/laboratorios")
+	for _, link := range links {
+		fmt.Println("XXX VISITING XXX")
+		c.Visit(link)
+		announcements = nil
+	}
 	fmt.Print("Done\n")
 }
