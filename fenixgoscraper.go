d49a62373d503eda264473fff7b89f7242b32d83
Woodwood1299 <jmtd242@gmail.com>
2025-09-15 18:15:37 +0100

    Added Scrape Function

--- fenixgoscraper.go
+++ fenixgoscraper.go
@@ -1,5 +1,38 @@
 package fenixgoscraper
 
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+type Announcement struct {
+	Link string
+}
+
 func ScrapeOut() string {
 	return "I'M SCRAPING\n"
+}
+
+func Scrape() {
+	var announcements []Announcement
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("fenix.tecnico.ulisboa.pt/disciplinas"))
+
+	c.Visit("fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/laboratorios")
+
+	c.OnHTML("content-block", func(h *colly.HTMLElement) {
+		announcement := Announcement{}
+
+		announcement.Link = h.ChildAttr("a", "href")
+
+		announcements = append(announcements, announcement)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		for i := 0; i < len(announcements); i++ {
+			fmt.Print(announcements[i].Link)
+		}
+	})
 }
