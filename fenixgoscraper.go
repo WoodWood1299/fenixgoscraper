5fba59360c72f3d914675d6ba7a8c463f9837938
Woodwood1299 <jmtd242@gmail.com>
2025-09-15 18:37:06 +0100

    Debug Prints

--- fenixgoscraper.go
+++ fenixgoscraper.go
@@ -15,12 +15,15 @@ func ScrapeOut() string {
 }
 
 func Scrape() {
+	fmt.Print("Running\n")
+
 	var announcements []Announcement
 
 	c := colly.NewCollector(
 		colly.AllowedDomains("fenix.tecnico.ulisboa.pt/disciplinas"))
 
 	c.OnHTML("h5[style=margin-top: 0; font-weight:400]", func(h *colly.HTMLElement) {
+		fmt.Print("OnHTML\n")
 		announcement := Announcement{}
 
 		announcement.Link = h.ChildAttr("a", "href")
@@ -29,11 +32,13 @@ func Scrape() {
 	})
 
 	c.OnScraped(func(r *colly.Response) {
+		fmt.Print("OnScraped\n")
 		for i := 0; i < len(announcements); i++ {
 			fmt.Print(announcements[i].Link)
 		}
 	})
 
+	fmt.Print("Visiting\n")
 	c.Visit("fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/laboratorios")
-	print("Testing")
+	fmt.Print("Done\n")
 }
