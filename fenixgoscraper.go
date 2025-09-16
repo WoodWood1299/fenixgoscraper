15cd847138f7bbf26f788dc1a5b93d79f76739f6
Woodwood1299 <jmtd242@gmail.com>
2025-09-16 01:37:48 +0100

    Error Loging

--- fenixgoscraper.go
+++ fenixgoscraper.go
@@ -1,6 +1,7 @@
 package fenixgoscraper
 
 import (
+	"errors"
 	"fmt"
 	"html"
 
@@ -15,25 +16,29 @@ func ScrapeOut() {
 	fmt.Print("TEST\n")
 }
 
-func Scrape() {
-	fmt.Print("Running\n")
-	links := [4]string{
-		"https://fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/rss/announcement",
-		"https://fenix.tecnico.ulisboa.pt/disciplinas/Apre2222/2025-2026/1-semestre/rss/announcement",
-		"https://fenix.tecnico.ulisboa.pt/disciplinas/OC112/2025-2026/1-semestre/rss/announcement",
-		"https://fenix.tecnico.ulisboa.pt/disciplinas/RC112/2025-2026/1-semestre/rss/announcement",
+func Scrape(links []string) (string, error) {
+
+	if len(links) == 0 {
+		return "", errors.New("link array cannot be empty")
 	}
 
+	var out string
 	fp := gofeed.NewParser()
 
 	for _, l := range links {
-		feed, _ := fp.ParseURL(l)
-		fmt.Printf("%s\n", feed.Title)
+		feed, err := fp.ParseURL(l)
+
+		if err != nil {
+			return "", errors.New("error parsing RSS")
+		}
+
+		out += fmt.Sprintf("%s\n", feed.Title)
 
 		items := feed.Items
 		for _, a := range items {
-			fmt.Printf("- %s\n\t%s\n\n", html.UnescapeString(a.Title), a.Link)
+			out += fmt.Sprintf("- %s\n\t%s\n\n", html.UnescapeString(a.Title), a.Link)
 		}
 	}
-	fmt.Print("Done\n")
+
+	return out, nil
 }
--- go.mod
+++ go.mod
@@ -2,27 +2,15 @@ module github.com/WoodWood1299/fenixgoscraper
 
 go 1.25.1
 
-require github.com/gocolly/colly v1.2.0
+require github.com/mmcdole/gofeed v1.3.0
 
 require (
 	github.com/PuerkitoBio/goquery v1.10.3 // indirect
 	github.com/andybalholm/cascadia v1.3.3 // indirect
-	github.com/antchfx/htmlquery v1.3.4 // indirect
-	github.com/antchfx/xmlquery v1.4.4 // indirect
-	github.com/antchfx/xpath v1.3.5 // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/mmcdole/gofeed v1.3.0 // indirect
 	github.com/mmcdole/goxpp v1.1.1-0.20240225020742-a0c311522b23 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
-	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.44.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.36.9 // indirect
 )
